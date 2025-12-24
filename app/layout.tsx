6cdde1bf83edcea90d23239358b60aa0b7379400
Harsh Pandey <harshpandeyiitian04@gmail.com>
2025-12-24 21:05:51 +0530

    added whatsapp

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
+import ChatWidget from "@/components/ChatWidget";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,8 @@ export default function RootLayout({
 
         <Header />
         {children}
+
+        <ChatWidget />
         <Footer />
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,6 @@ export default function Home() {
       <Client/>
       <Insight/>
       <Contact/>
-      <ChatWidget />
     </>
   );
 }
