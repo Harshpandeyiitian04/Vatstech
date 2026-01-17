853ac0138990a008043f20c27ab6c09e6980d222
Harsh Pandey <harshpandeyiitian04@gmail.com>
2026-01-17 18:23:59 +0530

    added

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output:'export',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output:'export'
 };
 
 export default nextConfig;
